b47fda13eb011a03558c263b8aa796b2f98888ba
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-03 12:36:03 -0200

    added an option parser

--- dd.rb
+++ dd.rb
@@ -1,8 +1,32 @@
 require "digest"
 require 'find'
+require 'optparse'
 
-path = File.dirname(__FILE__)
-Find.find(path) do |file|
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: #{__FILE__} [options]"
+
+  opts.on('-d', '--directory DIR', 'The directory to scan') do |arg|
+    options[:directory] = arg
+  end
+end
+
+begin
+  optparse.parse!
+  mandatory = [:directory]
+  missing = mandatory.select{ |param| options[param].nil? }
+  if not missing.empty?
+    puts "Missing options: #{missing.join(', ')}"
+    puts optparse
+    exit
+  end
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts $!.to_s
+  puts optparse
+  exit
+end
+
+Find.find(options[:directory]) do |file|
 	next unless File.file?(file)
 	digest = Digest::SHA256.file(file).hexdigest
 	puts "#{file} - #{digest}"
